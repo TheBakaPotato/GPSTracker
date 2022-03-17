80bd099d6491df3d37b55aba039860ddb20fe479
TheBakaPotato <TheBakaPotato@thebakapotato.com>
2022-03-16 21:28:23 -0300

    Another step/video update

--- app/src/main/java/com/example/gpstracker/MainActivity.java
+++ app/src/main/java/com/example/gpstracker/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.gpstracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
     public static final int DEFAUL_INTERVAL = 30;
     public static final int FASTEST_INTERVAL = 5;
+    private static final int PERMISSIONS_FINE_LOCATION = 99;
     TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
     Switch sw_locationsupdates, sw_gps;
     FusedLocationProviderClient fusedLocationProviderClient;
@@ -37,5 +48,71 @@ public class MainActivity extends AppCompatActivity {
         locationReqest.setInterval(1000 * DEFAUL_INTERVAL);
         locationReqest.setFastestInterval(1000 * FASTEST_INTERVAL);
         locationReqest.setInterval(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        sw_gps.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                if (sw_gps.isChecked()){
+                    locationReqest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                    tv_sensor.setText("Using GPS sensors");
+                }else {
+                    locationReqest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+                    tv_sensor.setText("Using tower signal + wifi");
+
+                }
+            }
+        });
+        updateGPS();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMISSIONS_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    updateGPS();
+                }else{
+                    Toast.makeText(this, "This app needs permission to run", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    private void updateGPS(){
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                updateUIValues(location);
+
+            }
+        });
+        }else{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
+            }
+
+        }
+    }
+
+    private void updateUIValues(Location location) {
+        tv_lat.setText(String.valueOf(location.getLatitude()));
+        tv_lon.setText(String.valueOf(location.getLongitude()));
+        tv_accuracy.setText(String.valueOf(location.getAccuracy()));
+        if (location.hasAccuracy()==true){
+            tv_altitude.setText(String.valueOf(location.getAltitude()));
+        }else{
+            tv_altitude.setText("Not available");
+        }
+        if (location.hasSpeed()==true){
+            tv_speed.setText(String.valueOf(location.getSpeed()));
+        }else{
+            tv_speed.setText("Not available");
+        }
+
     }
 }
